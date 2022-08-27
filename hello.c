f85913879644d4ca98838c42855666d24263dec8
AndrewDuw <aduweling2@huskers.unl.edu>
2022-08-27 21:30:46 +0000

    added my minor

--- hello.c
+++ hello.c
@@ -8,6 +8,7 @@ int main(int argc, char **argv) {
 
     printf("Hello, I'm Andrew Duweling!\n");
     printf("Major is Computer Engineering!\n");
+    printf("Minor is Not applicable\n");
 
     return 0;
 }
