f115459872056fef76ed93e7c78e6dfd4d77a577
NickWayman <nwayman2@huskers.unl.edu>
2022-08-27 21:56:14 +0000

    Nick added major

--- hello.c
+++ hello.c
@@ -7,8 +7,9 @@
 int main(int argc, char **argv) {
 
     printf("Hello, I'm Andrew Duweling!\n");
+    printf("Hi, I'm Nick Wayman and I am majoring in Chemical Engineering!\n");
     printf("Major is Computer Engineering!\n");
-    printf("Minor is Not applicable\n");
+    printf("Minor is Not applicable to Andrew and Nick has a CS minor and Physics minor\n");
 
     return 0;
 }
